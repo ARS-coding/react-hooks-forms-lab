de504427023f988a7c95c0763f4df3f60fdd95d8
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-24 14:49:37 +0300

    Add filter and search features.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onSearchChange, onCategoryChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" onChange={onSearchChange} placeholder="Search..." />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
+        <option value="Produce" selected>Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
       </select>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,24 +4,39 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const [selectedCategory, setSelectedCategory] = useState("Produce");
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchTextChange(event) {
+    setSearchText(event.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  function displayItems() {
+    const categoryItems = items.filter((item) => {
+      if (selectedCategory === "All") return true;
+  
+      return item.category === selectedCategory;
+    });
+  
+    function searchItems() { // don't call this if searchText state is an empty string
+      let regex = new RegExp(`${searchText}`, "i");
+      console.log(regex)
+      return categoryItems.filter((item) => regex.test(item.name)) // if the item object's name property contains the given searchText inside, it will return it
+    }
+    return searchItems()
+  }
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onSearchChange={handleSearchTextChange} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {displayItems().map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
